378af6a22b4cd491b83f40dfdb25fdbad86d9326
Roman Pogorelov <rmk140987@gmail.com>
2020-07-11 11:03:00 +0300

    Fixed by remarks: fixed form animation.

--- js/script.js
+++ js/script.js
@@ -6,19 +6,20 @@ if (openButton && searchForm) {
   openButton.addEventListener("click", function (evt) {
     evt.preventDefault();
     searchForm.classList.toggle("modal-show");
+    searchForm.classList.remove("modal-error");
   });
+}
 
-  if (searchButton) {
-    searchButton.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      let dateIn = document.querySelector(".date-in-input").value;
-      let dateOut = document.querySelector(".date-out-input").value;
-      let adultCount = document.querySelector(".adult-div-count input").value;
-      if (!(dateIn && dateOut && adultCount)) {
-        searchForm.classList.remove("modal-error");
-        searchForm.offsetWidth = searchForm.offsetWidth;
-        searchForm.classList.add("modal-error");
-      }
-    })
-  }
+if (searchButton) {
+  searchButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    searchForm.classList.remove("modal-error");
+    let dateIn = document.querySelector(".date-in-input").value;
+    let dateOut = document.querySelector(".date-out-input").value;
+    let adultCount = document.querySelector(".adult-div-count input").value;
+    if (!(dateIn && dateOut && adultCount)) {
+      searchForm.offsetWidth = searchForm.offsetWidth;
+      searchForm.classList.add("modal-error");
+    }
+  })
 }
